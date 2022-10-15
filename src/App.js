c65733c7570151142950e13421d2eeffa229aa99
Xavicklo <Andy286460@gmail.com>
2022-10-15 14:34:48 +0800

    Init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import Cyberpunk from "./data/Cyberpunk.jpg";
+import moon from "./data/moon.jpg";
+
+function App() {
+  return <img src={moon} className="cyberpunk" />;
+}
+
+export default App;
